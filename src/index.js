cdb6b2c27d3d97b084c460a5184e610c82d7761e
Saurabh Kushwaha <sk2005@gmail.com>
2025-05-22 10:22:34 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-// const {User} = require('./models/index');
-// const bcrypt = require('bcrypt');
-// const UserRepository = require('./repository/user-repository');
+// const UserService = require('./services/user-service');
 
 const app = express();
 
@@ -19,12 +17,12 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on port: ${PORT}`);
-        // const repo = new UserRepository();
-        // const response = await repo.getById(1);
-        // console.log(response);
-        // const incomingpassword = '123456';
-        // const  user = await User.findByPk(3);
-        // const response = bcrypt.compareSync(incomingpassword, user.password);
+
+        //const service = new UserService();
+        // const newToken = service.createToken({email: 'saurabh@admin.com', id: 1});
+        // console.log("new token is:",newToken);
+        // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhdXJhYmhAYWRtaW4uY29tIiwiaWQiOjEsImlhdCI6MTc0Nzg4Njk0NCwiZXhwIjoxNzQ3ODg2OTc0fQ.6jqrzS6fSrSkU2JkxVLTsXXsjpCvxr1wkuAoR6gDEa0';
+        // const response = service.verifyToken(token);
         // console.log(response);
     });
 }
